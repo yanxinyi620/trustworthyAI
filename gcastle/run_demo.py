46a8f01291ecec5d4282dc6417d09fdf402cf759
yanxinyi <41045439+yanxinyi620@users.noreply.github.com>
2021-10-29 18:20:21 +0800

    Update run_demo.py

--- gcastle/run_demo.py
+++ gcastle/run_demo.py
@@ -4,13 +4,13 @@ from castle.common import GraphDAG
 from castle.metrics import MetricsDAG
 from castle.datasets import DAG, IIDSimulation
 
-true_dag, X = load_dataset(name='iid_test')
+# true_dag, X = load_dataset(name='iid_test')
 
-# weighted_random_dag = DAG.erdos_renyi(n_nodes=10, n_edges=12, weight_range=(0.5, 2.0), seed=300)
-# dataset = IIDSimulation(W=weighted_random_dag, n=2000, method='linear', sem_type='gauss')
-# true_dag, X = dataset.B, dataset.X
+weighted_random_dag = DAG.erdos_renyi(n_nodes=10, n_edges=10, weight_range=(0.5, 2.0), seed=1)
+dataset = IIDSimulation(W=weighted_random_dag, n=2000, method='nonlinear', sem_type='quadratic')
+true_dag, X = dataset.B, dataset.X
 
-n = RL(lambda_flag_default=True, nb_epoch=1000)
+n = RL(lambda_flag_default=True, nb_epoch=20000)
 
 n.learn(X, dag=true_dag)
 
